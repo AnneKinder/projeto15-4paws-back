32a83a6d6f87143eda7470de3c1d07e4514a996a
AnneKin <annekhd98@gmail.com>
2022-11-23 19:01:35 -0300

    feat: config init

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { MongoClient } from 'mongodb'
+
+const app = express()
+dotenv.config()
+app.use(express.json())
+
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+
+try{
+    await mongoClient.connect()
+}
+catch(err){
+    console.log(err)
+}
+
+let db = mongoClient.db("4paws")
+
+
+
+
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
