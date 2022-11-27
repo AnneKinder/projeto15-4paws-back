5628897d9671230cd8ae1bd384457df2c857a0e5
AnneKin <annekhd98@gmail.com>
2022-11-26 22:47:22 -0300

    feat: tempcart post

--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { home } from "../controllers/homeController.js";
+import { home, postTempCart } from "../controllers/homeController.js";
 const homeRouter = Router();
 
 homeRouter.get("/home", home);
+homeRouter.post("/home", postTempCart);
+
 
 
 export default homeRouter;
